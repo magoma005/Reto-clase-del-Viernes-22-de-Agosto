3c8f27927965bc496efb7039ab447654b32f162e
Miguel Angel Guarin <mago123005@gmail.com>
2025-08-22 16:53:40 -0500

    first commit

---
+++ reto.py
@@ -0,0 +1,9 @@
+nombre = input("Digite su nombre ")
+#Mayuscula
+print(nombre.upper())
+#Minuscula
+print(nombre.lower())
+#Caracteres en total
+print(nombre.__len__())
+#Primera letra y ultima letra
+print(nombre [0] + nombre [5])
